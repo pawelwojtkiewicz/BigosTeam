3c40c45141383d1efb5fc139f184d49c7ef3c8b7
atalanau <aliaksandr.talanau@ice.com>
2024-09-14 23:09:14 +0200

    added api med-kit/closest

--- backend/src/api/med-kit/services/med-kit.js
+++ backend/src/api/med-kit/services/med-kit.js
@@ -5,5 +5,63 @@
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
+const haversine = require('haversine');
 
-module.exports = createCoreService('api::med-kit.med-kit');
+module.exports = createCoreService('api::med-kit.med-kit', ({ strapi }) => ({
+	async findClosest(ctx) {
+	  let { lat, long, distance, ...serviceCtx } = ctx;
+
+	  if (!distance) {
+		distance = 50;
+	  }
+  
+	  // fetch all available results
+	  const medkits = await strapi.entityService.findMany('api::med-kit.med-kit', {
+		fields: ['lat', 'long'],
+		filters: {
+		  $and: [
+			{
+			  lat: {
+				$notNull: true
+			  }
+			},
+			{
+			  long: {
+				$notNull: true
+			  }
+			}
+		  ]
+		},
+		...(serviceCtx.locale
+		  ? { locale: serviceCtx.locale }
+		  : {}
+		)
+	  });
+  
+	  function createCoordsTuple(lat, long) {
+		return [parseFloat(lat), parseFloat(long)];
+	  }
+  
+	  let currentDistance;
+  
+	  const availableMedkits = medkits.filter(medkit => {
+		currentDistance = haversine(
+		  createCoordsTuple(lat, long),
+		  createCoordsTuple(medkit.lat, medkit.long),
+		  {
+			format: '[lat,lon]',
+			unit: 'meter'
+		  }
+		);
+		console.log(currentDistance);
+		return currentDistance < distance;
+	  });
+  
+	  return {
+		entity: availableMedkits,
+		meta: {
+		  distance
+		}
+	  };
+	}
+  }));
