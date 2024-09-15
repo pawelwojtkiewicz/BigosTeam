6788e1ca0006ee862dd9ceb5d6a2a8c2bd68cba4
atalanau <aliaksandr.talanau@ice.com>
2024-09-15 15:00:51 +0200

    applied map and donation to mainpage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,121 +1,75 @@
-import Image from "next/image";
+"use client";
+
 import Link from 'next/link';
 import {
-    UserGroupIcon,
-    HomeIcon,
+    UserIcon
 } from '@heroicons/react/24/outline';
 import styles from "./page.module.css";
-import MainPageContent from './components/MainPageContent'
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import MapPage from './mappage/page';
+import Blik from './components/Blik/Blik';
+import Image from 'next/image';
 
 const links = [
-  { name: 'Home', href: '/home', icon: HomeIcon },
-  { name: 'Main', href: '/', icon: UserGroupIcon },
+  { name: 'Login', href: '/login', icon: UserIcon }
 ];
 
-export default async function Home() {
+export default function Home() {
+  const [isBlikVisible, setIsBlikVisible] = useState(true);
+  
   const link = links[0]
   const LinkIcon = link.icon;
+
+  const toggleBlikVisibility = () => {
+    setIsBlikVisible(!isBlikVisible);
+  };
 
   return (
     <div id="main-page" className={styles.page}>
       <Suspense fallback={<div>powolutku ładuje</div>}>
-        <MainPageContent />
+        <MapPage />
       </Suspense>
       <Link
             key={link.name}
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+			style={{
+				position: 'absolute',
+				top: '15px',
+				right: '15px',
+				zIndex: 1000,
+				background: '#000055',
+				padding: '10px',
+				borderRadius: '8px',
+			  }}
           >
             <LinkIcon className="w-6" />
-          <p className="hidden md:block">{link.name}</p>
+          <span className="hidden">{link.name}</span>
       </Link>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+	  <div style={{
+        position: 'absolute',
+        bottom: '15px',
+        right: '15px',
+        backgroundColor: 'white',
+        padding: isBlikVisible ? '15px' : '0',
+        borderRadius: '8px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        zIndex: 1000, // Added z-index
+      }}>
+        <button onClick={toggleBlikVisibility} style={{
+          background: 'none',
+          border: 'none',
+          cursor: 'pointer',
+          position: 'absolute',
+          top: isBlikVisible ? '0' : '-50px',
+          right: '0',
+          padding: '10px',
+		  color: 'black',
+        }}>
+          {isBlikVisible ? 'Close' : <Image src={BlikSvg} alt="Show Blik" width={100} height={24} />}
+        </button>
+        {isBlikVisible && <Blik />} {/* Conditionally render the Blik component */}
+      </div>
     </div>
   )
 }
